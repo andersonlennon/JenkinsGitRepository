2e1b0787e671f1588fdd8330ad37335a7eb0fd4f
Anderson <andersonlennonsk8@gmail.com>
2018-03-03 19:20:25 -0300

    first commit

---
+++ src/MyPackage/smokeTest.java
@@ -0,0 +1,24 @@
+package MyPackage;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class smokeTest {
+	
+	@Test 
+	public void loginTest() throws InterruptedException{
+//		WebDriver driver = new FirefoxDriver();
+//		driver.get("http://rediff.com");
+//		driver.manage().window().maximize();
+//		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//		driver.findElement(By.xpath("//*[@id='signin_info']/a[1]")).click();
+//		Thread.sleep(3000);
+//		driver.quit();
+		System.out.println("Testing");
+	}
+
+}
